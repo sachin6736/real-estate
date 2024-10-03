3b3cdec8c41864f6f2daad53e8747b34950b9394
Sachin A <sachinpradeepan27@gmail.com>
2024-10-03 12:33:04 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
+export default function App(){
+   return <BrowserRouter>
+   <Header/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/About' element={<About/>}/>
+    <Route path='/Profile' element={<Profile/>}/>
+    <Route path='/Sign-In' element={<SignIn/>}/>
+    <Route path='/Sign-Up' element={<SignUp/>}/>
+   </Routes>
+   </BrowserRouter>
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdOutlineManageSearch } from "react-icons/md";
+
+export default function Header() {
+  return (
+    <header className='bg-slate-400'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+           <Link to='/'>
+            <h1 className='font-bold'>
+                <span>Find</span>
+                <span className='text-blue-700'>H</span>
+            </h1>
+            </Link>
+            <form className='relative'>
+                <input
+                    className='rounded-sm p-2 pl-10 focus:outline-blue-600 w-24 sm:w-64'
+                    type='text'
+                    placeholder='search..'
+                />
+                <MdOutlineManageSearch className='absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-700 text-xl' />
+            </form>
+            <ul className='flex gap-3'>
+            <Link to='/'>
+                <li className='hover:underline text-blue-700 font-bold'>Home</li>
+                </Link>
+                <Link to='/About'>
+                <li className='hidden sm:inline hover:underline ' >About</li>
+                </Link>
+                <Link to='/Sign-In'>
+                <li className='hover:underline' >SignIN</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  );
+}
