1c222fd786e6830f5c055aeb7c70471875ea39c6
Sachin A <sachinpradeepan27@gmail.com>
2024-10-03 14:19:06 +0530

    Connect MongoDB database to the project

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express(); 
+mongoose.connect(process.env.mong).then(()=>{
+    console.log("connected to mongo db")
+}).catch((err)=>{
+    console.log(err)
+})
+const app = express();  
 
 app.listen(3000,()=>{
     console.log("server running 3000 !")
